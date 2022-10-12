fa0e8cc24943a48ceffba284f558cd2809d328b8
BUNTY KUMAR SHARMA <quamatbanty01@gmail.com>
2022-10-12 08:40:02 -0700

    Inside func

--- js/script.js
+++ js/script.js
@@ -14,19 +14,17 @@ const viewBtn = document.querySelector(".view-modal"),
 
     setTimeout(
       function(){
-        pop = ()=>{
-          input.select(); //select input value
-          if(document.execCommand("copy")){ //if the selected text copy
-            field.classList.add("active");
-            copy.innerText = "Copied";
-            setTimeout(()=>{
-              window.getSelection().removeAllRanges(); //remove selection from document
-              field.classList.remove("active");
-              copy.innerText = "Copy";
-            }, 3000);
-          }
+        input.select(); //select input value
+        if(document.execCommand("copy")){ //if the selected text copy
+          field.classList.add("active");
+          copy.innerText = "Copied";
+          setTimeout(()=>{
+            window.getSelection().removeAllRanges(); //remove selection from document
+            field.classList.remove("active");
+            copy.innerText = "Copy";
+          }, 3000);
         }
-        }
+      }
       ,10000 /// milliseconds = 10 seconds
     );
-   
+    
