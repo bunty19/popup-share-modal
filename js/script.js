b7493745c6c994fbb68e4435e6923d21b14857c7
BUNTY KUMAR SHARMA <quamatbanty01@gmail.com>
2022-10-12 08:33:55 -0700

    add timeout

--- js/script.js
+++ js/script.js
@@ -12,15 +12,21 @@ const viewBtn = document.querySelector(".view-modal"),
       viewBtn.click();
     }
 
-    copy.onclick = ()=>{
-      input.select(); //select input value
-      if(document.execCommand("copy")){ //if the selected text copy
-        field.classList.add("active");
-        copy.innerText = "Copied";
-        setTimeout(()=>{
-          window.getSelection().removeAllRanges(); //remove selection from document
-          field.classList.remove("active");
-          copy.innerText = "Copy";
-        }, 3000);
-      }
-    }
+    setTimeout(
+      function(){
+        pop = ()=>{
+          input.select(); //select input value
+          if(document.execCommand("copy")){ //if the selected text copy
+            field.classList.add("active");
+            copy.innerText = "Copied";
+            setTimeout(()=>{
+              window.getSelection().removeAllRanges(); //remove selection from document
+              field.classList.remove("active");
+              copy.innerText = "Copy";
+            }, 3000);
+          }
+        }
+        }
+      ,10000 /// milliseconds = 10 seconds
+    );
+   
