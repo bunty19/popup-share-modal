afc74184dd38fa0174e22d0fd735899248795ec9
BUNTY KUMAR SHARMA <quamatbanty01@gmail.com>
2022-10-12 08:44:28 -0700

    On click

--- js/script.js
+++ js/script.js
@@ -5,26 +5,27 @@ const viewBtn = document.querySelector(".view-modal"),
     input = field.querySelector("input"),
     copy = field.querySelector("button");
 
-    viewBtn.onclick = ()=>{
-      popup.classList.toggle("show");
-    }
+
     close.onclick = ()=>{
       viewBtn.click();
     }
 
     setTimeout(
       function(){
-        input.select(); //select input value
-        if(document.execCommand("copy")){ //if the selected text copy
-          field.classList.add("active");
-          copy.innerText = "Copied";
-          setTimeout(()=>{
-            window.getSelection().removeAllRanges(); //remove selection from document
-            field.classList.remove("active");
-            copy.innerText = "Copy";
-          }, 3000);
-        }
+        popup.classList.toggle("show");
       }
       ,10000 /// milliseconds = 10 seconds
     );
-    
+
+    copy.onclick = ()=>{
+      input.select(); //select input value
+      if(document.execCommand("copy")){ //if the selected text copy
+        field.classList.add("active");
+        copy.innerText = "Copied";
+        setTimeout(()=>{
+          window.getSelection().removeAllRanges(); //remove selection from document
+          field.classList.remove("active");
+          copy.innerText = "Copy";
+        }, 3000);
+      }
+    }
